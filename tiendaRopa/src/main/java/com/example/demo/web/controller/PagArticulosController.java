fa530ab38efc2def1f102fa1beea10feafa926a2
Ikers890 <73229485+Ikers890@users.noreply.github.com>
2023-04-17 20:10:47 +0200

    Pagina de tienda funcional

--- tiendaRopa/src/main/java/com/example/demo/web/controller/PagArticulosController.java
+++ tiendaRopa/src/main/java/com/example/demo/web/controller/PagArticulosController.java
@@ -1,5 +1,7 @@
 package com.example.demo.web.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +9,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.model.dto.CategoriaDTO;
+import com.example.demo.model.dto.ProductoDTO;
+import com.example.demo.services.CategoriaService;
+import com.example.demo.services.ProductoService;
 import com.example.demo.services.UsuarioService;
 
 @Controller
 public class PagArticulosController {
+	
 	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+	
+	@Autowired
+	private ProductoService ps;
+	
+	@Autowired
+	private CategoriaService cs;
 	
 	@GetMapping("/tienda")
 	public ModelAndView inicioTienda() {
 
 		log.info("PagArticulosController - PagArticulosController: Pasamos a la página con todos los artículos");
+		
+		List<ProductoDTO> lpDTO = ps.findAll();
+		List<CategoriaDTO> lcDTO = cs.findAll();
 
 		ModelAndView mav = new ModelAndView("clientes/tienda");
+		mav.addObject("listaProductosDTO", lpDTO);
+		mav.addObject("listaCategoriasDTO", lcDTO);
 
 		return mav;
 	}
