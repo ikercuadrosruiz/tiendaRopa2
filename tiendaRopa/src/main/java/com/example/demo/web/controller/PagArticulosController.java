5e23f62e6c3b84485bdd9dcbc2d36b05fe54aa76
Ikers890 <73229485+Ikers890@users.noreply.github.com>
2023-04-18 23:59:45 +0200

    Adicion de la ventana de mostrar articulo

--- tiendaRopa/src/main/java/com/example/demo/web/controller/PagArticulosController.java
+++ tiendaRopa/src/main/java/com/example/demo/web/controller/PagArticulosController.java
@@ -7,8 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.model.dto.CarritoProductoDTO;
 import com.example.demo.model.dto.CategoriaDTO;
 import com.example.demo.model.dto.ProductoDTO;
 import com.example.demo.services.CategoriaService;
@@ -29,7 +31,7 @@ public class PagArticulosController {
 	@GetMapping("/tienda")
 	public ModelAndView inicioTienda() {
 
-		log.info("PagArticulosController - PagArticulosController: Pasamos a la página con todos los artículos");
+		log.info("PagArticulosController - inicioTienda: Pasamos a la página con todos los artículos");
 		
 		List<ProductoDTO> lpDTO = ps.findAll();
 		List<CategoriaDTO> lcDTO = cs.findAll();
@@ -40,4 +42,33 @@ public class PagArticulosController {
 
 		return mav;
 	}
+	
+	@GetMapping("/tienda/producto/{idProducto}")
+	public ModelAndView verProducto(@PathVariable Long idProducto) {
+
+		log.info("PagArticulosController - verProducto: Pasamos a la página de ver el articulo con id " + idProducto );
+		
+		ProductoDTO pDTO = ps.findById(idProducto);
+
+		ModelAndView mav = new ModelAndView("clientes/mostrarArt");
+		mav.addObject("productoDTO", pDTO);
+
+		return mav;
+	}
+	
+	/*
+	@GetMapping("/trabajadores/categorias/delete/{idCategoria}")
+	public ModelAndView addCart(@PathVariable Long idCategoria) {
+		
+		log.info("PagArticulosController - addCart: Añadimos el producto al carrito");
+		
+		//Añadir producto al carrito ************* falta por hacer
+		List<CarritoProductoDTO>
+		
+		categoriaService.deleteById(idCategoria);
+		
+		ModelAndView mav = new ModelAndView("redirect:/trabajadores/categorias");
+		return mav;
+	}
+	*/
 }
