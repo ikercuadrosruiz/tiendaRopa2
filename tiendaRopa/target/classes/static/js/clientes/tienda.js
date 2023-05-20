e687d7ab36c3b5d21e83508fa7eb35da4e32c78b
Ikers890 <73229485+Ikers890@users.noreply.github.com>
2023-05-20 18:49:04 +0200

    Cambio de algunas imagenes

--- tiendaRopa/target/classes/static/js/clientes/tienda.js
+++ tiendaRopa/target/classes/static/js/clientes/tienda.js
@@ -5,12 +5,31 @@ var selectOptions = selectWrapper.querySelector('.select-options');
 
 // Mostrar/ocultar opciones al hacer clic en el select
 selectSelected.addEventListener('click', function() {
-  selectOptions.style.display = (selectOptions.style.display === 'block') ? 'none' : 'block';
+	selectOptions.style.display = (selectOptions.style.display === 'block') ? 'none' : 'block';
 });
 
 // Manejar la selección de una opción
 selectOptions.addEventListener('click', function(e) {
-  var selectedOption = e.target.textContent;
-  selectSelected.textContent = selectedOption;
-  selectOptions.style.display = 'none';
+	var selectedOption = e.target.textContent;
+	selectSelected.textContent = selectedOption;
+	selectOptions.style.display = 'none';
 });
+
+
+// Impedir arrastrar imagenes
+const imageProducto = document.querySelectorAll('#cont--producto img:nth-of-type(2)');
+imageProducto.forEach(image => {
+  image.addEventListener('dragstart', bloquearArrastre);
+});
+
+const imageHeart = document.querySelectorAll('#cont--producto img:nth-of-type(1)');
+imageHeart.forEach(image => {
+  image.addEventListener('dragstart', bloquearArrastre);
+});
+
+
+function bloquearArrastre(event) {
+  event.preventDefault();
+}
+
+
